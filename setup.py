ed2e3217b6d3cf1fdca303cb52a9456ef1684771
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2017-07-14 14:45:59 +0000

    version 0.4.11.99

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ install_requires = [
 
 setup(
     name='chainercv',
-    version='0.4.11',
+    version='0.4.11.99',
     packages=find_packages(),
     author='Yusuke Niitani',
     author_email='yuyuniitani@gmail.com',
